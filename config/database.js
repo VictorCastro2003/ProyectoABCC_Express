5ce4052ad895fe258036cf968432fb61fd6744d3
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-03-07 09:32:41 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,10 +3,14 @@
 const mysql = require('mysql');
 
 const conexion= mysql.createConnection({
-    host:'localhost',
-    user:'victorcastro',
-    password:'itsj',
-    database:'BD_Express_2025'
+ const connection = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "mi_base_de_datos",
+  port: process.env.DB_PORT || 3306,
+});
+
 
 });
 conexion.connect(function(err){
