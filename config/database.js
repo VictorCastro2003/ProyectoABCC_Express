28b55b7a25e7dfa970d0b3afd16a3d92db8429d7
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-03-08 21:01:23 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,37 +1,16 @@
-const mysql = require("mysql");
+'use strict';
 
-const dbConfig = {
-  host: process.env.MYSQLHOST, 
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
-};
+const mysql = require('mysql');
 
-let connection;
+const conexion= mysql.createConnection({
+    host:'localhost',
+    user:'victorcastro',
+    password:'itsj',
+    database:'BD_Express_2025'
 
-function handleDisconnect() {
-  connection = mysql.createConnection(dbConfig);
-
-  connection.connect((err) => {
-    if (err) {
-      console.error("🚨 Error al conectar a la BD:", err);
-      setTimeout(handleDisconnect, 5000); // Reintentar conexión en 5 segundos
-    } else {
-      console.log("✅ Conexión exitosa a la BD");
-    }
-  });
-
-  connection.on("error", (err) => {
-    console.error("🚨 Error en la conexión:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect(); // Reconectar en caso de desconexión
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+});
+conexion.connect(function(err){
+    if(err)throw err;
+    console.log('Conexion Exitosas a BD');
+});
+module.exports=conexion;
