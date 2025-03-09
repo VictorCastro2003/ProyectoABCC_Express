7612f8c8bf9a024ac2745b7b4d4ae75d91ddb38b
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-03-08 21:40:56 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,13 +2,13 @@
 
 const mysql = require('mysql');
 
-const conexion= mysql.createConnection({
-    host:'localhost',
-    user:'victorcastro',
-    password:'itsj',
-    database:'BD_Express_2025'
-
+const conexion = mysql.createConnection({
+    host: 'sql3.freesqldatabase.com', 
+    user: 'sql3766662',           
+    password: '9rtfPPsExy',       
+    database: 'sql3766662'          
 });
+
 conexion.connect(function(err){
     if(err)throw err;
     console.log('Conexion Exitosas a BD');
